6f7504300e8e090d1b1c7445b7a4ad67d4e3c2f2
Sultan2k7 <sarysultanbek2007@gmail.com>
2025-01-22 00:05:22 +0500

    upd: move to app from input

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpException, HttpStatus} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,21 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Post()
+    async handleInput(@Body() data: any) {
+      try {
+        await this.appService.processInput(data);
+        return { message: 'Data processed successfully' };
+      } catch (error) {
+        this.handleError(error);
+      }
+    }
+  
+    private handleError(error: any): void {
+      console.error('Error:', error.message);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(error.message || 'Bad Request', HttpStatus.BAD_REQUEST);
+    }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { UserController } from './user/user.controller';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { VehicleController } from './vehicle/vehicle.controller';
 import { GpsModule } from './gps/gps.module';
-import { InputModule } from './input/input.module'
+
 
 @Module({
   imports: [
@@ -23,7 +23,6 @@ import { InputModule } from './input/input.module'
     DatabaseModule,
     VehicleModule,
     GpsModule,
-    InputModule
   ],
   controllers: [AppController, AuthController, UserController, VehicleController],
   providers: [AppService, DatabaseService],
